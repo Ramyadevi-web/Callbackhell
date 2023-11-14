ade073ef0b999ede9ca5ee3187f7643f05b2b5d2
Ramyadevi-web <ramyadevim7@gmail.com>
2023-11-14 14:16:10 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+setTimeout(()=>{
+    countdown.style.color = "green";
+    document.getElementById("countdown").innerText = "10";
+setTimeout(()=>{
+    countdown.style.color = "orange";
+    document.getElementById("countdown").innerText = "9";
+},2000)
+setTimeout(()=>{
+    countdown.style.color = "green";
+    document.getElementById("countdown").innerText = "8";
+},3000)
+setTimeout(()=>{
+    countdown.style.color = "orange";
+    document.getElementById("countdown").innerText = "7";
+},4000)
+setTimeout(()=>{
+    countdown.style.color = "green";
+    document.getElementById("countdown").innerText = "6";
+},5000)
+setTimeout(()=>{
+    countdown.style.color = "orange";
+    document.getElementById("countdown").innerText = "5";
+},6000)
+setTimeout(()=>{
+    countdown.style.color = "green";
+    document.getElementById("countdown").innerText = "4";
+},7000)
+setTimeout(()=>{
+    countdown.style.color = "orange";
+    document.getElementById("countdown").innerText = "3";
+},8000)
+setTimeout(()=>{
+    countdown.style.color = "green";
+    document.getElementById("countdown").innerText = "2";
+},9000)
+setTimeout(()=>{
+    countdown.style.color = "orange";
+    document.getElementById("countdown").innerText = "1";
+},10000)
+setTimeout(()=>{
+    countdown.style.color = "green";
+    countdown.style.border = "4px solid orange";
+    countdown.style.borderRadius = "20px";
+    countdown.style.width = "530px";
+    countdown.style.marginLeft="320px";
+    document.getElementById("countdown").innerText = "Happy Independence Day!";
+},11000)
+},1000);
+
