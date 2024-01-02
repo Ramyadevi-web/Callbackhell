522d73783d2e772b672031b659a4caf44cd53dd4
Ramyadevi-web <ramyadevim7@gmail.com>
2024-01-02 19:37:36 +0530

    callback hell acheived

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
+
 setTimeout(()=>{
     countdown.style.color = "green";
     document.getElementById("countdown").innerText = "10";
-setTimeout(()=>{
-    countdown.style.color = "orange";
-    document.getElementById("countdown").innerText = "9";
-},2000)
-setTimeout(()=>{
-    countdown.style.color = "green";
-    document.getElementById("countdown").innerText = "8";
-},3000)
-setTimeout(()=>{
-    countdown.style.color = "orange";
-    document.getElementById("countdown").innerText = "7";
-},4000)
-setTimeout(()=>{
-    countdown.style.color = "green";
-    document.getElementById("countdown").innerText = "6";
-},5000)
-setTimeout(()=>{
-    countdown.style.color = "orange";
-    document.getElementById("countdown").innerText = "5";
-},6000)
-setTimeout(()=>{
-    countdown.style.color = "green";
-    document.getElementById("countdown").innerText = "4";
-},7000)
-setTimeout(()=>{
-    countdown.style.color = "orange";
-    document.getElementById("countdown").innerText = "3";
-},8000)
-setTimeout(()=>{
-    countdown.style.color = "green";
-    document.getElementById("countdown").innerText = "2";
-},9000)
-setTimeout(()=>{
-    countdown.style.color = "orange";
-    document.getElementById("countdown").innerText = "1";
-},10000)
-setTimeout(()=>{
-    countdown.style.color = "green";
-    countdown.style.border = "4px solid orange";
-    countdown.style.borderRadius = "20px";
-    countdown.style.width = "530px";
-    countdown.style.marginLeft="320px";
-    document.getElementById("countdown").innerText = "Happy Independence Day!";
-},11000)
+    setTimeout(()=>{
+        countdown.style.color = "orange";
+        document.getElementById("countdown").innerText = "9";
+        setTimeout(()=>{
+            countdown.style.color = "green";
+            document.getElementById("countdown").innerText = "8";
+            setTimeout(()=>{
+                countdown.style.color = "orange";
+                document.getElementById("countdown").innerText = "7";
+         
+            setTimeout(()=>{
+                countdown.style.color = "green";
+                document.getElementById("countdown").innerText = "6";
+        
+            setTimeout(()=>{
+                countdown.style.color = "orange";
+                document.getElementById("countdown").innerText = "5";
+                setTimeout(()=>{
+                    countdown.style.color = "green";
+                    document.getElementById("countdown").innerText = "4";
+                    setTimeout(()=>{
+                        countdown.style.color = "orange";
+                        document.getElementById("countdown").innerText = "3";
+                        setTimeout(()=>{
+                            countdown.style.color = "green";
+                            document.getElementById("countdown").innerText = "2";
+                            setTimeout(()=>{
+                                countdown.style.color = "orange";
+                                document.getElementById("countdown").innerText = "1";
+                                setTimeout(()=>{
+                                     countdown.style.color = "orange";                                    
+                                     countdown.style.color = "green";
+                                     countdown.style.border = "4px solid orange";
+                                     countdown.style.borderRadius = "20px";
+                                     countdown.style.width = "530px";
+                                     countdown.style.marginLeft="320px";
+                                    document.getElementById("countdown").innerText = "Happy Independence Day";
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+},1000)
 },1000);
+
 
